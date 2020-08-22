2d7e9d552dfff07df07b8db86854aa17c85ae291
Spencer Ye <57966319+TheSteak-dev@users.noreply.github.com>
2020-08-21 21:56:34 -0700

    Update GraphicsFrame.java

--- src/workout/GraphicsFrame.java
+++ src/workout/GraphicsFrame.java
@@ -1,6 +1,10 @@
 package workout;
 
+import javax.swing.JFrame;
+
 public class GraphicsFrame {
+	
+	JFrame frame;
 
 	public GraphicsFrame() {
 		// TODO Auto-generated constructor stub
@@ -8,7 +12,14 @@ public class GraphicsFrame {
 	
 	public void start()
 	{
+		frame = new JFrame("Workout Generator");
 		
+		
+		 
+		
+		frame.setBounds(100, 100, 500, 500);
+		frame.setResizable(false);
+		frame.setVisible(true);
 	}
 
 }
