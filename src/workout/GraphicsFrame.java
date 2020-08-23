2f6cf5322b5e28bb2f750d07b5ca307ec69d79ad
Spencer Ye <57966319+TheSteak-dev@users.noreply.github.com>
2020-08-22 22:05:21 -0700

    Add files via upload

--- src/workout/GraphicsFrame.java
+++ src/workout/GraphicsFrame.java
@@ -1,25 +1,69 @@
 package workout;
 
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 
 public class GraphicsFrame {
 	
 	JFrame frame;
+	
+	JPanel menuPage, loadSavedPage, loadPresetPage, loadRandomPage, currentPage = null, lastPage = null;
+	
+	JButton loadSaved, loadPreset, loadRandom;
+	
+	Container pane;
+	
 
 	public GraphicsFrame() {
-		// TODO Auto-generated constructor stub
+		frame = new JFrame("Workout Generator");
+		 pane = frame.getContentPane();
 	}
 	
 	public void start()
 	{
-		frame = new JFrame("Workout Generator");
 		
 		
+		menuPage = new JPanel();
+		loadSavedPage = new JPanel();
+		loadPresetPage = new JPanel();
+		loadRandomPage = new JPanel();
+		
+		loadSaved = new JButton("Load Saved");
+		loadSaved.setBounds(100, 100, 130, 30);
+		loadSaved.addActionListener(new ActionListener() {@Override	public void actionPerformed(ActionEvent arg0) {goToScreen(loadSavedPage);}});
+		
+		loadPreset = new JButton("Load Pre-Set");
+		loadPreset.setBounds(100, 200, 130, 30);
+		loadPreset.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent arg0) {goToScreen(loadPresetPage);}});
+		
+		loadRandom = new JButton("Load Pre-Set");
+		loadRandom.setBounds(100, 300, 130, 30);
+		loadRandom.addActionListener(new ActionListener() {@Override public void actionPerformed(ActionEvent arg0) {goToScreen(loadPresetPage);}});
 		 
+		currentPage = menuPage;
+		pane.add(menuPage);
+		
+		menuPage.add(loadSaved);
+		menuPage.add(loadPreset);
+		menuPage.add(loadRandomPage);
+		
+		
 		
 		frame.setBounds(100, 100, 500, 500);
 		frame.setResizable(false);
 		frame.setVisible(true);
+	}
+	
+	private void goToScreen(JPanel panel)
+	{
+		pane.remove(currentPage);
+		pane.add(panel);
 	}
 
 }
