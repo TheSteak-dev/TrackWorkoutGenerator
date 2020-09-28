7e4cec003c47cacd583bb67f7d02d1551ae8fb5a
Spencer Ye <sye348@student.fuhsd.org>
2020-09-28 13:47:27 -0700

    doc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"workout","l":"GraphicsFrame"},{"p":"workout","l":"Ladder"},{"p":"workout","l":"LogWriter"},{"p":"workout","l":"Main"},{"p":"workout","l":"PanelStack"},{"p":"workout","l":"Rep"},{"p":"workout","l":"Set"},{"p":"workout","l":"Straight"},{"p":"workout","l":"Workout"}]
