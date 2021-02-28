dde4def14c09e6e346c095bfc6e7598f9345dcf5
Spencer Ye <sye348@student.fuhsd.org>
2021-02-27 19:48:30 -0800

    start laying out ml command

--- src/runners/MLMain.java
+++ src/runners/MLMain.java
@@ -1,9 +1,16 @@
 package runners;
 
+import java.io.File;
+
 public class MLMain 
 {
 	public static void main(String[] args)
 	{
-		
+		File directory = new File("C:\\Users\\skywa\\Documents\\Java Code\\WorkoutGenerator\\TrackWorkoutGenerator\\TrackWorkoutGenerator\\res\\premade");
+		String[] files = directory.list();
+		for (int i = 0; i < files.length - 1; i++) //-1 to remove Difficulty.txt
+		{
+			//TODO unpack info
+		}
 	}
 }
