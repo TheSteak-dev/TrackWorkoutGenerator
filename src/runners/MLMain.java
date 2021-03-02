b125450c0ad0a73f0c08c77fb34c9b299c5bc25d
Spencer Ye <sye348@student.fuhsd.org>
2021-03-01 22:42:59 -0800

    Update MLMain.java

--- src/runners/MLMain.java
+++ src/runners/MLMain.java
@@ -8,9 +8,11 @@ public class MLMain
 	{
 		File directory = new File("C:\\Users\\skywa\\Documents\\Java Code\\WorkoutGenerator\\TrackWorkoutGenerator\\TrackWorkoutGenerator\\res\\premade");
 		String[] files = directory.list();
-		for (int i = 0; i < files.length - 1; i++) //-1 to remove Difficulty.txt
+		for (int i = 0; i < files.length - 1; i++)
 		{
 			//TODO unpack info
 		}
+		File difficulties = new File("C:\\Users\\skywa\\Documents\\Java Code\\WorkoutGenerator\\TrackWorkoutGenerator\\TrackWorkoutGenerator\\res\\premade\\difficulties.txt");
+		
 	}
 }
