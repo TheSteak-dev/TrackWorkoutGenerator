6377dff6026536d2cdf0881decb9e0b2c25062e4
Spencer Ye <sye348@student.fuhsd.org>
2021-03-02 21:55:15 -0800

    set up parameters

--- src/runners/MLMain.java
+++ src/runners/MLMain.java
@@ -4,11 +4,29 @@ import java.io.File;
 
 public class MLMain 
 {
+	private static double[] y;
+	private static double[][] x;
+	private static double[] theta;
+	private static double alpha;
+	
+	private static int premadeNum = 0;
+	
+	private static final int parameters = 4;
+	
 	public static void main(String[] args)
 	{
 		File directory = new File("C:\\Users\\skywa\\Documents\\Java Code\\WorkoutGenerator\\TrackWorkoutGenerator\\TrackWorkoutGenerator\\res\\premade");
 		String[] files = directory.list();
-		for (int i = 0; i < files.length - 1; i++)
+		
+		premadeNum = files.length - 1;
+		
+		y = new double[premadeNum];
+		x = new double[premadeNum][parameters];
+		theta = new double[parameters];
+		alpha = 0.1;
+		
+		
+		for (int i = 0; i < premadeNum; i++)
 		{
 			//TODO unpack info
 		}
