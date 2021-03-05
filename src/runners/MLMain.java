7de219023cbf52c7cb47e90f759991ba614c194a
Spencer Ye <sye348@student.fuhsd.org>
2021-03-04 19:50:13 -0800

    Update MLMain.java

--- src/runners/MLMain.java
+++ src/runners/MLMain.java
@@ -2,6 +2,8 @@ package runners;
 
 import java.io.File;
 
+import datastructure.Workout;
+
 public class MLMain 
 {
 	private static double[] y;
@@ -13,23 +15,27 @@ public class MLMain
 	
 	private static final int parameters = 4;
 	
+	private static Workout[] presetWorkouts;
+	
 	public static void main(String[] args)
 	{
-		File directory = new File("C:\\Users\\skywa\\Documents\\Java Code\\WorkoutGenerator\\TrackWorkoutGenerator\\TrackWorkoutGenerator\\res\\premade");
-		String[] files = directory.list();
 		
-		premadeNum = files.length - 1;
+		File preset = new File ("res/premade");
+		premadeNum = presetWorkouts.length - 1;
+		
+		presetWorkouts = new Workout[premadeNum];
 		
 		y = new double[premadeNum];
 		x = new double[premadeNum][parameters];
 		theta = new double[parameters];
 		alpha = 0.1;
 		
-		
 		for (int i = 0; i < premadeNum; i++)
 		{
+			presetWorkouts[i] = new Workout(preset.listFiles()[i].toString());
 			//TODO unpack info
 		}
+		
 		File difficulties = new File("C:\\Users\\skywa\\Documents\\Java Code\\WorkoutGenerator\\TrackWorkoutGenerator\\TrackWorkoutGenerator\\res\\premade\\difficulties.txt");
 		
 	}
