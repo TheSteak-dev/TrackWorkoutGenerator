091117797c371d18a05ba368f34e4d239b11329f
Spencer Ye <sye348@student.fuhsd.org>
2021-03-23 21:02:23 -0700

    add new parameters

--- src/runners/MLMain.java
+++ src/runners/MLMain.java
@@ -44,7 +44,7 @@ public class MLMain
 		presetWorkouts = new Workout[premadeNum];
 		
 		y = new double[premadeNum];
-		x = new double[premadeNum * maxSets][parameters];
+		x = new double[premadeNum * maxSets][parameters + parameters ^ 2];
 		theta = new double[maxSets];
 		alpha = 0.01;
 		
@@ -57,10 +57,27 @@ public class MLMain
 			Set[] s = temp.getSets();
 			for (int j = 0; i < s.length; j++)
 			{
-				x[premadeNum * i + j][0] = s[j].getDistance();
-				x[premadeNum * i + j][1] = s[j].getRest();
-				x[premadeNum * i + j][2] = s[j].getType();
-				x[premadeNum * i + j][3] = s[j].getReps().length;
+				Set set = s[j];
+				x[premadeNum * i + j][0] = set.getDistance();
+				x[premadeNum * i + j][1] = set.getRest();
+				x[premadeNum * i + j][2] = set.getType();
+				x[premadeNum * i + j][3] = set.getReps().length;
+				x[premadeNum * i + j][4] = set.getDistance() * set.getDistance();
+				x[premadeNum * i + j][5] = set.getRest() * set.getDistance();
+				x[premadeNum * i + j][6] = set.getType() * set.getDistance();
+				x[premadeNum * i + j][7] = set.getReps().length * set.getDistance();
+				x[premadeNum * i + j][8] = set.getDistance() * set.getRest();
+				x[premadeNum * i + j][9] = set.getRest() * set.getRest();
+				x[premadeNum * i + j][10] = set.getType() * set.getRest();
+				x[premadeNum * i + j][11] = set.getReps().length * set.getRest();
+				x[premadeNum * i + j][12] = set.getDistance() * set.getType();
+				x[premadeNum * i + j][13] = set.getRest() * set.getType();
+				x[premadeNum * i + j][14] = set.getType() * set.getType();
+				x[premadeNum * i + j][15] = set.getReps().length * set.getType();
+				x[premadeNum * i + j][16] = set.getDistance() * set.getReps().length;
+				x[premadeNum * i + j][17] = set.getRest() * set.getReps().length;
+				x[premadeNum * i + j][18] = set.getType() * set.getReps().length;
+				x[premadeNum * i + j][19] = set.getReps().length * set.getReps().length;
 			}
 		}
 		
