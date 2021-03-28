1a000dc90a35400814c467a74046228ea8fe5522
Spencer Ye <sye348@student.fuhsd.org>
2021-03-28 16:09:24 -0700

    fix another bug

--- src/runners/MLMain.java
+++ src/runners/MLMain.java
@@ -64,26 +64,26 @@ public class MLMain
 			for (int j = 0; j < s.length; j++)
 			{
 				Set set = s[j];
-				x[premadeNum * i + j][0] = set.getDistance();
-				x[premadeNum * i + j][1] = set.getRest();
-				x[premadeNum * i + j][2] = set.getType();
-				x[premadeNum * i + j][3] = set.getReps().length;
-				x[premadeNum * i + j][4] = set.getDistance() * set.getDistance();
-				x[premadeNum * i + j][5] = set.getRest() * set.getDistance();
-				x[premadeNum * i + j][6] = set.getType() * set.getDistance();
-				x[premadeNum * i + j][7] = set.getReps().length * set.getDistance();
-				x[premadeNum * i + j][8] = set.getDistance() * set.getRest();
-				x[premadeNum * i + j][9] = set.getRest() * set.getRest();
-				x[premadeNum * i + j][10] = set.getType() * set.getRest();
-				x[premadeNum * i + j][11] = set.getReps().length * set.getRest();
-				x[premadeNum * i + j][12] = set.getDistance() * set.getType();
-				x[premadeNum * i + j][13] = set.getRest() * set.getType();
-				x[premadeNum * i + j][14] = set.getType() * set.getType();
-				x[premadeNum * i + j][15] = set.getReps().length * set.getType();
-				x[premadeNum * i + j][16] = set.getDistance() * set.getReps().length;
-				x[premadeNum * i + j][17] = set.getRest() * set.getReps().length;
-				x[premadeNum * i + j][18] = set.getType() * set.getReps().length;
-				x[premadeNum * i + j][19] = set.getReps().length * set.getReps().length;
+				x[maxSets * i + j][0] = set.getDistance();
+				x[maxSets * i + j][1] = set.getRest();
+				x[maxSets * i + j][2] = set.getType();
+				x[maxSets * i + j][3] = set.getReps().length;
+				x[maxSets * i + j][4] = set.getDistance() * set.getDistance();
+				x[maxSets * i + j][5] = set.getRest() * set.getDistance();
+				x[maxSets * i + j][6] = set.getType() * set.getDistance();
+				x[maxSets * i + j][7] = set.getReps().length * set.getDistance();
+				x[maxSets * i + j][8] = set.getDistance() * set.getRest();
+				x[maxSets * i + j][9] = set.getRest() * set.getRest();
+				x[maxSets * i + j][10] = set.getType() * set.getRest();
+				x[maxSets * i + j][11] = set.getReps().length * set.getRest();
+				x[maxSets * i + j][12] = set.getDistance() * set.getType();
+				x[maxSets * i + j][13] = set.getRest() * set.getType();
+				x[maxSets * i + j][14] = set.getType() * set.getType();
+				x[maxSets * i + j][15] = set.getReps().length * set.getType();
+				x[maxSets * i + j][16] = set.getDistance() * set.getReps().length;
+				x[maxSets * i + j][17] = set.getRest() * set.getReps().length;
+				x[maxSets * i + j][18] = set.getType() * set.getReps().length;
+				x[maxSets * i + j][19] = set.getReps().length * set.getReps().length;
 			}
 		}
 		System.out.println(MachineLearningFunctions.calculateLinearCost(theta, x, y));
